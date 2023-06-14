6a8ae9c5e27c3f8c85f0938bad169c00c741a78a
Ben Gibbons <ben@MacBook-Pro.local>
2023-06-14 21:15:33 +0100

    Adding demo files

---
+++ demo/build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+#create the docker image 
+docker build -t springboot_app .
+
+
+#create the sprinboard application 
+./run.sh
+
+
+
+
+
+
+
---
+++ demo/run.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#Create a new docker container
+docker run --name bg_springboot_app -p 80:8080 springboot_app
+
+
+#Run the gradle booRun command to start the springboot web application
+./gradlew clean
+./gradlew bootRun
